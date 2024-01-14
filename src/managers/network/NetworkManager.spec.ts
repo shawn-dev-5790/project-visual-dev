f791887d3f4846dc7975944f0e47f30745fc614b
shawn-dev-5790 <shawn.dev.5790@gmail.com>
2024-01-14 21:06:48 +0900

    fix: :art: 네트워크 메니저 테스트 코드 추가

--- src/managers/network/NetworkManager.spec.ts
+++ src/managers/network/NetworkManager.spec.ts
@@ -1,9 +1,48 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { test, describe } from 'vitest'
+import { test, describe, expect } from 'vitest'
+import NetworkManager from './NetworkManager'
+
+interface IReqGetTestData {
+  method: 'GET'
+  url: 'https://reqres.in/api/users/2' | string
+}
+interface IResGetTestData {
+  data: {
+    id: number
+    email: string
+    first_name: string
+    last_name: string
+    avatar: string
+  }
+  support: {
+    url: string
+    text: string
+  }
+}
 
 describe('NetworkManager', () => {
-  test('creates a request using axios instance', () => {
-    console.log('\x1b[48;5;214m\x1b[30m[TODO:] NetworkManager test should be added\x1b[0m')
+  test('creates a request using axios instance', async () => {
+    const req = () => {
+      return NetworkManager.create<IReqGetTestData, IResGetTestData>({
+        method: 'GET',
+        url: 'https://reqres.in/api/users/2',
+      })
+    }
+
+    const res = await req()
+    expect(res.data).toEqual({
+      data: {
+        id: 2,
+        email: 'janet.weaver@reqres.in',
+        first_name: 'Janet',
+        last_name: 'Weaver',
+        avatar: 'https://reqres.in/img/faces/2-image.jpg',
+      },
+      support: {
+        url: 'https://reqres.in/#support-heading',
+        text: 'To keep ReqRes free, contributions towards server costs are appreciated!',
+      },
+    })
   })
 })
